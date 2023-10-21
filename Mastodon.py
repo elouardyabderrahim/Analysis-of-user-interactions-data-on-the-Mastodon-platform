2118f873a8bd6cecde82f2e49fb74556891d5f80
elouardyabderrahim <elouardy.abderrahim07@gmail.com>
2023-10-21 23:50:59 +0100

    scrapping script

--- Mastodon.py
+++ Mastodon.py
@@ -1,4 +1,7 @@
+
+
 from mastodon import Mastodon;
+from hdfs import InsecureClient;
 import outils
 mastodon = Mastodon(
     client_id=outils.CLIENT_ID,
@@ -7,15 +10,43 @@ mastodon = Mastodon(
     api_base_url="https://mastodon.social"
 )
 
-#get my account id
-my_account = mastodon.account_verify_credentials()
-print('this is the account id :',my_account.id)
 
-# Replace "target_username" with the username you want to search for
-account = mastodon.account_search("NanoRaptor")
+# Function to handle errors
+def handle_error(exception):
+    print(f"An error occurred: {str(exception)}")
 
-if account:
-    print("Account ID:", account[0].id)
-else:
-    print("User not found.")
+# Connect to HDFS
+hdfs_client = InsecureClient('http://localhost:9870', user='abderrahim')
+print("hdfs_client",hdfs_client)
+
+try:
+    # Retrieve a list of trending hashtags
+    trending_tags = mastodon.trending_tags()
+
+    # Iterate through the trending tags and fetch posts for each tag
+    for trending_tag in trending_tags:
+        hashtag = trending_tag['name']
+        hashtag_posts = mastodon.timeline_hashtag(hashtag)
+        print("hashtag_posts",hashtag_posts)
+        # Process the posts for the current trending tag and extract information as needed
+        for post in hashtag_posts:
+            # TODO :
+            post_id = post['id']
+            user_id = post['account']['id']
+
+            # Process the post and user information
+
+            # Store the data in HDFS
+            hdfs_directory = '/usr/local/hadoop/row'
+            hdfs_file_name = 'data.json'
+
+            with hdfs_client.write(f'{hdfs_directory}/{hdfs_file_name}', overwrite=True) as writer:
+                # Write the data to the HDFS file
+                data_to_write = f"Post ID: {post_id}, User ID: {user_id}"
+                writer.write(data_to_write)
+                print("writing is done!!")
+   
+except Exception as e:
+    handle_error(e)
+
 
